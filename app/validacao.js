b218388789e29ff87a65c58c5f4adfb35fd693c4
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-05 14:04:11 -0300

    Fix number bug NaN

--- app/validacao.js
+++ app/validacao.js
@@ -1,11 +1,17 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute;
-
+    
     if(chuteForInvalido(numero)) {
-        console.log("valor inválido");
+        elementoChute.innerHTML += "<div>Valor inválido</div>";
     }
     if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        console.log("Valor inválido");
+        elementoChute.innerHTML += `<div>Valor inválido: Diga um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}.</h3>
+        `
     }
 }
 
